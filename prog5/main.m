c88b93a60b9b38bfd986e96cd9363b5cddf153a7
bocheng <bocheng@bochengteki-MacBook-Pro.local>
2014-01-04 13:39:22 +0800

    Initial Commit

---
+++ prog5/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  prog5
+//
+//  Created by bocheng on 2014/1/4.
+//  Copyright (c) 2014年 bocheng. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
